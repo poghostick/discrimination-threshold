7fd1ac5860af16da595525b72e195cc61987cda0
poghostick <arsen.v.poghosyan@gmail.com>
2022-04-17 17:12:36 +0200

    Replaced dashboard in app.py with the bootstrap-based one

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+"""The module contains the function build_plot, which builds a 
+Plotly Dash application with an interactive DT Plot. The file
+can be run both from the bash command as well as be called by an
+instance of the InteractiveDiscriminationThreshold class from 
+the module interactive_discrimination_threshold.
+"""
+
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import flask
 from jupyter_dash import JupyterDash
 import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
+import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from typing import List, Union
 
 
-def build_plot(data: pd.DataFrame, width: int = 1200,
-               height: int = 550, test_size=0.2,
-               app_mode='jupyter_inline') -> Union[Dash, JupyterDash]:
+def build_plot(data: pd.DataFrame, test_size=0.2,
+               app_mode='external') -> Union[Dash, JupyterDash]:
     """Generates an interactive version of the discrimination threshold
     plot.
 
@@ -19,145 +25,199 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
     ----------
         data: pd.DataFrame
             Data to be used an input for the plot
-        width: int, optional
-            Width of the plot (default is 1200)
-        height: int, optional
-            Height of the plot (default is 550)
         test_size: float, optional
             Share of the test set (default is 0.2)
         app_mode: str, optional
                 If 'inline', the app is being created as instance of
                 JupyterDash and the plot is drawn inside the notebook; if
                 'external', then the app is created as JupyterDash
-                instance and the plot is drawn in separate window; when
-                'server' the app is created as Dash instance to be
+                instance and the plot is drawn in a separate window; if
+                'server', then the app is created as a Dash instance to be
                 deployed as a server application
-                (default is 'jupyter_inline')
+                (default is 'inline')
     Returns
     -------
         An instance of Dash or JupyterDash to be used for plotting.
     """
-    # Build all the widgets generating additional parameters for the plot
-    n_obs = int(data.shape[0] * test_size)
+
+    n_obs = int(data.shape[0])
+
     if app_mode in ('inline', 'external'):
-        app = JupyterDash(__name__)
+        app = JupyterDash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
     else:
         server = flask.Flask(__name__)
         app = Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])
-    app.layout = html.Div([
-        html.Div([
-            html.Div(
+
+    controls = [
+        dbc.Form(
+            [
+                dbc.Label("Interval"),
+                dcc.Dropdown(
+                            id='interval_dropdown',
+                            clearable=False,
+                            value=0.95,
+                            options=[
+                                {'label': f'{ii}%', 'value': ii / 100}
+                                for ii in [0, 5, 10, 25, 50, 75, 90, 95]
+                            ],
+                            style={'margin-bottom': '8px', 'width': '100px'}
+                        )]),
+        dbc.Form(
+            [
+                dbc.Label("Metrics"),
+                dbc.Checklist(
+                    options=[
+                        {'label': 'Precision', 'value': 'precision'},
+                        {'label': 'Recall', 'value': 'recall'},
+                        {'label': 'F1-score', 'value': 'f1'},
+                        {'label': 'Queue Rate', 'value': 'queue_rate'},
+                    ],
+                    value=['precision', 'recall', 'queue_rate'],
+                    id="metrics",
+                    inline=True,
+                    style={'margin-bottom': '8px'},
+                ),
+            ]
+        ),
+        dbc.Form(
+            [
+                dbc.Label('weight(Recall) / weight(Precision)'),
+                dbc.Input(
+                    id='recall_over_precision',
+                    type='number',
+                    min=0,
+                    step=0.1,
+                    value=1,
+                    style={'width': '100px', 'margin-bottom': '15px'},
+                ),
+            ]
+        ),
+        dbc.Form(
+            [
+            dbc.Checklist(
+                id='constrained',
+                options=[{'label': 'Enable Constrained Mode',
+                          'value': 'constrained'}],
+                value=[],
+            ),
+            
+            dbc.Form([
+                dbc.Label('Reviewing Capacity (Ratio)'),
+                dcc.Slider(min=0, max=n_obs, value=n_obs / 2, id='capacity'),
+                ]),
+            
+            ]
+        ),
+        dbc.Form(
+            [
+                dbc.Label('Cost of Reviewing per Unit'),
+                dbc.Input(
+                    id='cost_per_unit',
+                    type='number',
+                    min=0,
+                    step=0.5,
+                    value=0,
+                    placeholder='Enter',
+                    style={'width': '100px', 'margin-top': '10px', 'margin-bottom': '5px'},
+                ),
+            ],
+        ),
+        dbc.Form(
+            [
+                dbc.Label('Payout per Success (True Positive)'),
+                dbc.Input(
+                    id='payout_per_tp',
+                    type='number',
+                    min=0,
+                    step=0.5,
+                    value=0,
+                    placeholder='Enter',
+                    style={'width': '100px'},
+                ),
+            ],
+        ),
+        
+    ]
+
+    app.layout = dbc.Container(
+        fluid=True,
+        style={"height": "100vh"},
+        children=[
+            html.H1('Discrimination Threshold'),
+            html.Hr(),
+            dbc.Row(
                 [
-                    'Interval',
-                    dcc.Dropdown(
-                        id='interval_dropdown',
-                        clearable=False,
-                        value=0.95,
-                        options=[
-                            {'label': f'{ii}%', 'value': ii / 100}
-                            for ii in [0, 5, 10, 25, 50, 75, 90, 95]
-                        ],
-                        style={'margin-top': '10px'}
+                    dbc.Col(
+                        width=3,
+                        children=dbc.Card(
+                            [dbc.CardHeader("Controls"), dbc.CardBody(controls),]
+                        ),
+                    ),
+                    dbc.Col(
+                        width=9,
+                        children=dbc.Card(
+                            [
+                                dbc.CardHeader("The Plot"),
+                                dbc.CardBody(dcc.Graph(id='discrimination_threshold'),
+                                             style={'height': '100%'})
+                            ],
+                        ),
                     ),
                 ],
-                style={'width': '100px', 'display': 'inline-block',
-                       'margin-left': '5%', 'margin-top': '25px'}
             ),
-            html.Div(
-                dcc.Checklist(
-                    id='checklist',
-                    options=[
-                        {'label': ' Precision', 'value': 'precision'},
-                        {'label': ' Recall', 'value': 'recall'},
-                        {'label': ' F1 Score', 'value': 'f1'},
-                        {'label': ' Queue Rate', 'value': 'queue_rate'}],
-                    value=['precision', 'recall', 'queue_rate'],
-                    labelStyle={'display': 'block'},
-                ),
-                style={'width': '180px', 'display': 'inline-block',
-                       'margin-left': '7%', 'margin-top': '25px'}
-            ),
-            html.Div([
-                dcc.Checklist(
-                    id='payout',
-                    options=[{'label': ' Optimal Threshold by Screening'
-                              ' Capacity, Cost and Revenue', 'value':
-                                  'payout'}],
-                    value=[],
-                    labelStyle={'display': 'block',
-                                'margin-bottom': '10px'},
-                ),
-                html.Div([dcc.Slider(id='slider_id', min=0,
-                                     max=n_obs, value=n_obs / 2)],
-                         style={'width': '500px'}),
-                html.Div([
-                    html.Label('Cost of screening per unit'),
-                    dcc.Input(
-                        id='cost_per_unit',
-                        type='number',
-                        step=0.5,
-                        value=0,
-                        placeholder='Enter cost (+) per unit',
-                        style={'width': '190px', 'margin-bottom': '1%',
-                               'margin-left': '65px'})]),
-                html.Div([
-                    html.Label('Revenue / Loss per True Positive'),
-                    dcc.Input(
-                        id='revenue_loss',
-                        type='number',
-                        step=0.5,
-                        value=0,
-                        placeholder='Enter revenue (+) or loss (-)',
-                        style={'width': '190px', 'margin-bottom': '1%',
-                               'margin-left': '10px'})])],
-                style={'width': '50%', 'display': 'inline-block',
-                       'margin-top': '20px', 'margin-left': '5%'})],
-            style={'display': 'flex'},
-        ),
-        dcc.Graph(id='discrimination_threshold'),
-    ])
+        ],
+    )
 
     # The interface for interaction between widgets and the plot
     @app.callback(
         Output('cost_per_unit', 'disabled'),
-        Output('revenue_loss', 'disabled'),
-        Output('slider_id', 'disabled'),
-        Input('payout', 'value'),
+        Output('payout_per_tp', 'disabled'),
+        Output('capacity', 'disabled'),
+        Input('constrained', 'value'),
     )
-    def update_payout(payout: List[str]):
-        """Controls the possibility for inserting payout values when
-        when the payout checkbox is marked."""
-        if 'payout' not in payout:
+    def update_constrained(constrained: List[str]):
+        """Controls the possibility for inserting values for constrained mode
+        when the constrained checkbox is marked."""
+        if 'constrained' not in constrained:
             return (True, True, True)
         else:
             return (False, False, False)
-
+    
     @app.callback(
         Output('discrimination_threshold', 'figure'),
+        Input('recall_over_precision', 'value'),
         Input('interval_dropdown', 'value'),
-        Input('checklist', 'value'),
-        Input('payout', 'value'),
-        Input('slider_id', 'value'),
+        Input('metrics', 'value'),
+        Input('constrained', 'value'),
+        Input('capacity', 'value'),
         Input('cost_per_unit', 'value'),
-        Input('revenue_loss', 'value'))
-    def update_graph(interval_dropdown: int,
-                     checklist: List,
-                     payout: List,
-                     slider: int,
+        Input('payout_per_tp', 'value')
+    )
+    def update_graph(recall_over_precision: float,
+                     interval_dropdown: int,
+                     metrics: List,
+                     constrained: List,
+                     capacity: int,
                      cost_per_unit: float,
-                     revenue_loss: float):
-        """Draws the plot based on the widget values"""
+                     payout_per_tp: float):
+        """Draw the plot based on the widget values"""
+        if recall_over_precision != 1:
+            data['f1'] = 0
+            beta = recall_over_precision ** 2
+            data.loc[data['precision'] * data['recall'] != 0, 'f1'] = (
+                (1 + beta) * data['precision'] * data['recall'] 
+                / (beta * data['precision'] + data['recall'])
+            )
         df_grp = data.groupby('thresholds')
         thresholds = list(df_grp.groups.keys())
-        # widget_values = get_widget_values(widget)
         metric_title = {'precision': 'Precision', 'recall': 'Recall',
                         'queue_rate': 'Queue Rate', 'f1': 'F1-Score'}
         px_colors = px.colors.qualitative.G10
-        metrics = ['precision', 'recall', 'queue_rate', 'f1', 'payout']
+        all_metrics = ['precision', 'recall', 'queue_rate', 'f1', 'payout']
         metric_color = {metric: px_colors[metric_id] for metric_id,
-                        metric in enumerate(metrics)}
+                        metric in enumerate(all_metrics)}
         fig = make_subplots(specs=[[{"secondary_y": True}]])
-        for metric in checklist:
+        for metric in metrics:
             fig.add_trace(
                 go.Scatter(x=thresholds,
                            y=df_grp[metric].quantile(0.5),
@@ -198,10 +258,10 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
                                '<extra></extra>',
                                fill='tonexty'),
                     secondary_y=False)
-        if 'payout' in payout:
+        if 'constrained' in constrained:
             payout_median = (
-                (df_grp['precision'].quantile(0.5) * 100
-                 * revenue_loss - cost_per_unit)
+                (df_grp['precision'].quantile(0.5)
+                 * payout_per_tp- cost_per_unit)
                 * df_grp['queue_rate'].quantile(0.5) * n_obs)
             fig.add_trace(
                 go.Scatter(x=thresholds,
@@ -215,12 +275,12 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
                 secondary_y=True)
             if interval_dropdown > 0:
                 payout_min = (
-                    (df_grp['precision'].quantile(interval_min) * 100 *
-                     revenue_loss - cost_per_unit)
+                    (df_grp['precision'].quantile(interval_min)
+                     * payout_per_tp - cost_per_unit)
                     * df_grp['queue_rate'].quantile(interval_min) * n_obs)
                 payout_max = (
-                    (df_grp['precision'].quantile(interval_max) * 100
-                     * revenue_loss - cost_per_unit)
+                    (df_grp['precision'].quantile(interval_max) 
+                     * payout_per_tp - cost_per_unit)
                     * df_grp['queue_rate'].quantile(interval_max) * n_obs)
                 fig.add_trace(
                     go.Scatter(x=thresholds,
@@ -247,7 +307,7 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
                                fill='tonexty'),
                     secondary_y=True)
             fig.add_trace(
-                go.Scatter(x=[slider / n_obs, slider / n_obs],
+                go.Scatter(x=[capacity / n_obs, capacity / n_obs],
                            y=[0, 1],
                            mode='lines',
                            line=dict(color='black', dash='dot'),
@@ -255,7 +315,7 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
                            hovertemplate='Maximum Screening Capacity:'
                            ' %{x:.2f}<extra></extra>'),
                 secondary_y=False)
-            opt_threshold = payout_median[:(slider / n_obs)].idxmax()
+            opt_threshold = payout_median[:(capacity + 0.02 / n_obs)].idxmax()
         else:
             opt_threshold = df_grp['f1'].quantile(0.5).idxmax()
         fig.add_trace(
@@ -267,27 +327,28 @@ def build_plot(data: pd.DataFrame, width: int = 1200,
                        hovertemplate='Optimal Threshold: %{x:.2f}'
                        '<extra></extra>'),
             secondary_y=False),
-        fig.update_layout(title='Discrimination Threshold',
-                          title_x=0.08,
+        fig.update_layout(title_x=0.08,
                           title_font_size=30,
                           xaxis_title='threshold',
                           xaxis=dict(ticks='outside'),
+                          height=800,
                           autosize=True,
-                          width=width,
-                          height=height,
-                          template='plotly_white')
+                          template='plotly_white',
+                          legend=dict(
+                                orientation="h",
+                                yanchor="bottom",
+                                y=1.02,
+                                xanchor="right",
+                                x=1))
         fig.update_yaxes(title_text='score',
                          secondary_y=False)
         fig.update_yaxes(title_text='payout value',
                          secondary_y=True)
         return fig
-    
-    return app
-    
 
-if __name__ == '__main__':
-    # The function is called from here only when the app is deployed
-    # and ran from server
+    return app
+
+if __name__ == "__main__":
     data = pd.read_csv('data.csv')
-    app = build_plot(data=data, width=1400, height=900)
-    app.run_server(port=8051)
+    app = build_plot(data=data, app_mode='server')
+    app.run_server(debug=True, port=8051)
